1b056f4d8c652b181efdd06ca60dddb2e5d49d82
SNaweenkumAR <naween1924@gmail.com>
2025-02-03 09:53:03 +0530

    backend

--- src/Component/Contact.jsx
+++ src/Component/Contact.jsx
@@ -38,7 +38,7 @@ const handleSubmit = async (e) => {
     }
   
     try {
-      const response = await fetch("http://localhost:5000/api/contact", {
+      const response = await fetch(process.env.BACKEND_URL, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData),
